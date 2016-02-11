5329569704057bf93b059138c4d627292e822b2b
Alisa Prathnadi <aprathna@ucsd.edu>
2016-02-11 07:53:46 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -15,6 +15,12 @@ function initializePage() {
 }
 
 /*
+$("#testjs").click(function(e){
+	$.get("/project/1", addProjectDetails);
+})
+*/
+
+/*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
@@ -27,7 +33,25 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/"+ idNumber, addProject)
 }
+
+function addProject(result){
+	console.log(result);
+
+	var projectHTML = '<a href="#" class="thumbnail">' +
+		'<ima src="' + result['image'] + '" class="detailsImage">'+
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p></a>' +
+		'<p>' + result['summary'] + '</p>';
+
+	//$("#project-container").html(projectHTML);
+	//$("#project-description").html(result['summary']);	
+	$("#project" + result['id'] + " .details").html(projectHTML);
+}
+
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +59,20 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", colorClick);
 }
+
+function colorClick(result) {
+	console.log(result);
+
+	var color = result['colors'];
+	var colors = color.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
